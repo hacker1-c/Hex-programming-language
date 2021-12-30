4b080d72e9ab0421820c2ee8b3341fbadaa58a6e
hacker1-c <83634876+hacker1-c@users.noreply.github.com>
2021-12-30 14:08:49 +0700

    Create hex_language.py

---
+++ hex_language.py
@@ -0,0 +1,48 @@
+import socket
+import os
+
+
+class console:
+    def writeline(lines, text):
+        for i in range(lines):
+            print()
+        print(text)
+
+    def nextline(lines):
+        for i in range(lines):
+            print()
+
+    def write(text):
+        print(text)
+
+    def clear():
+        os.system('clear')
+
+
+class user:
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+
+
+def IFc(condition, code):
+    exec("""if {}:(
+      {})""".format(condition, code))
+
+
+class command:
+    def newFolder(folder_name):
+        os.mkdir(folder_name)
+
+    def deleteFolder(folder_name):
+        os.rmdir(folder_name)
+
+    def delete(filename):
+        os.remove(filename)
+
+    def rename(filename):
+        os.rename(filename)
+
+
+def forever(code):
+    while True:
+        exec(code)
